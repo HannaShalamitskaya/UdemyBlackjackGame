fc407f87335a531a49aa32e3ccb1710a7338c173
Hanna <Hanna_Shalamitskaya@epam.com>
2019-10-14 22:37:16 +0300

    Add initial code source

---
+++ source/game.py
@@ -0,0 +1,7 @@
+"""A module with BalckJack game."""
+
+
+class Game:
+    def __init__(self, player_1_name='player_1', player_2_name='player_2'):
+        self.player_1_name = player_1_name
+        self.player_2_name = player_2_name
