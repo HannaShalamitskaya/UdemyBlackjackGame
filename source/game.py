00d813e37a55d9fc4d03eda36550ffb085f80639
Hanna <Hanna_Shalamitskaya@epam.com>
2019-10-14 23:10:42 +0300

    Update CI

--- source/game.py
+++ source/game.py
@@ -3,5 +3,5 @@
 
 class Game:
     def __init__(self, player_1_name='player_1', player_2_name='player_2'):
-        self.player_1_name = player_1_name
-        self.player_2_name = player_2_name
+        self.player_1 = player_1_name
+        self.player_2 = player_2_name
