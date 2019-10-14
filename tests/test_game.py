6493e484088c5f8329dee4a7a52b955d227adf60
Hanna <Hanna_Shalamitskaya@epam.com>
2019-10-14 23:22:56 +0300

    Update CI config

--- tests/test_game.py
+++ tests/test_game.py
@@ -6,5 +6,5 @@ from source.game import Game
 def test_game():
     game = Game('player_1', 'player_2')
 
-    assert game.player_1._name == 'player_1'
-    assert game.player_2._name == 'player_2'
+    assert game.player_1 == 'player_1'
+    assert game.player_2 == 'player_2'
